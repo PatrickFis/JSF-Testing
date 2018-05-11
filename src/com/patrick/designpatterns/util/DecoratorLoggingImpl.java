6933f0dd0d77ad26ffc9797e8faebc126e2ac609
PatrickFis <fischerpl@mail.lipscomb.edu>
2018-05-10 21:45:37 -0500

    Removed unnecessary constructor

--- src/com/patrick/designpatterns/util/DecoratorLoggingImpl.java
+++ src/com/patrick/designpatterns/util/DecoratorLoggingImpl.java
@@ -2,10 +2,6 @@ package com.patrick.designpatterns.util;
 
 public class DecoratorLoggingImpl implements DecoratorLoggingInterface {
 	
-	public DecoratorLoggingImpl() {
-		// Inject me
-	}
-	
 	@Override
 	public void log(String message) {
 		System.out.println(message);
